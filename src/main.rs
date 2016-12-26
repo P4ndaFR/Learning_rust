56fa77c8160fa6b380cf1f4c1af13140e9b99e70
nd4pa <antoine.blondeau@isen-bretagne.fr>
2016-12-26 02:07:00 +0100

    Initial commit : Complete guess game

---
+++ src/main.rs
@@ -0,0 +1,49 @@
+extern crate rand;
+
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
+
+fn main()
+{
+    println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1,101);
+
+    //Dev print pour test
+    //println!("The secret number is {}", secret_number);
+
+    loop
+    {
+        println!("Please input your guess.");
+
+        //Instanciation d'une variable changeable, qui es liée à une chaîne vide.
+        let mut guess = String::new();
+
+        //Récupération du stdin + \n
+        io::stdin().read_line(&mut guess)
+            .expect("Failed to read the line");
+
+        /*Cast propre de String à u32 => suppression des espaces et des \n, puis conversion.
+        Si c'est bien un nombre, pas de réalise la conversion, sinon, skip le reste de l'itération*/
+        let guess: u32 = guess.trim()
+                              .parse()
+        {
+            Ok(num) => num,
+            Err(_) => continue,
+        }
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&secret_number)
+        {
+            Ordering::Less      => println!("Too small!"),
+            Ordering::Greater   => println!("Too big!"),
+            Ordering::Equal     =>
+            {
+                println!("You win!");
+                break;
+            }
+        }
+    }
+
+}
